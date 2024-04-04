e0b76a294b6b8d7b79e03300b5e068e63d6fd183
rrvv123 <eorms7391@gmail.com>
2024-04-04 14:56:48 +0900

    안드로이드 응용 프로그램 개발 - 0404 6교시

--- app/src/main/java/kr/ac/kopo/simplecalculater0328/MainActivity.kt
+++ app/src/main/java/kr/ac/kopo/simplecalculater0328/MainActivity.kt
@@ -2,9 +2,11 @@ package kr.ac.kopo.simplecalculater0328
 
 import android.icu.number.IntegerWidth
 import android.os.Bundle
+import android.view.View.OnClickListener
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,8 +19,9 @@ class MainActivity : AppCompatActivity() {
     lateinit var btnMinus:Button
     lateinit var btnMultiply:Button
     lateinit var btnDivide:Button
+    lateinit var btnMod:Button
     lateinit var textResult:TextView
-    var result : Int? = null
+    var result : Double? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,36 +35,71 @@ class MainActivity : AppCompatActivity() {
         btnMinus = findViewById(R.id.btnMinus)
         btnMultiply = findViewById(R.id.btnMultiply)
         btnDivide = findViewById(R.id.btnDivide)
+        btnMod = findViewById(R.id.btnMod)
 
         textResult = findViewById<TextView>(R.id.textResult)
 
-        btnPlus.setOnClickListener {
-            var num1 = edit1.text.toString().toInt()
-            var num2 = edit2.text.toString().toInt()
-            var result = num1 + num2
-            textResult.text = "계산 결과: " + result
-            false
+//        btnPlus.setOnClickListener {
+//            var num1 = edit1.text.toString().toInt()
+//            var num2 = edit2.text.toString().toInt()
+//            var result = num1 + num2
+//            textResult.text = "계산 결과: " + result
+//            false
+//        }
+//        btnMinus.setOnClickListener {
+//            var num1 = edit1.text.toString().toInt()
+//            var num2 = edit2.text.toString().toInt()
+//            var result = num1 - num2
+//            textResult.text = "계산 결과: " + result
+//            false
+//        }
+//        btnMultiply.setOnClickListener {
+//            var num1 = edit1.text.toString().toInt()
+//            var num2 = edit2.text.toString().toInt()
+//            var result = num1 * num2
+//            textResult.text = "계산 결과: " + result
+//            false
+//        }
+//        btnDivide.setOnClickListener {
+//            var num1 = edit1.text.toString().toInt()
+//            var num2 = edit2.text.toString().toInt()
+//            var result = num1 / num2
+//            textResult.text = "계산 결과: " + result
+//            false
+//        }
+        btnPlus.setOnClickListener(btnListener)
+        btnMinus.setOnClickListener(btnListener)
+        btnMultiply.setOnClickListener(btnListener)
+        btnDivide.setOnClickListener(btnListener)
+        btnMod.setOnClickListener(btnListener)
+    }
+
+    var btnListener = OnClickListener{
+        var num1Str = edit1.text.toString().toString()
+        var num2Str = edit2.text.toString().toString()
+
+        if(num1Str=="" || num2Str==""){
+            Toast.makeText(baseContext, "입력을 하지 않았습니다.", Toast.LENGTH_LONG).show()
+            return@OnClickListener
         }
-        btnMinus.setOnClickListener {
-            var num1 = edit1.text.toString().toInt()
-            var num2 = edit2.text.toString().toInt()
-            var result = num1 - num2
-            textResult.text = "계산 결과: " + result
-            false
+
+        var num1 = edit1.text.toString().toDouble()
+        var num2 = edit2.text.toString().toDouble()
+
+        when(it.id){
+            R.id.btnPlus -> result = num1 + num2
+            R.id.btnMinus -> result = num1 - num2
+            R.id.btnMultiply -> result = num1 * num2
+            R.id.btnDivide -> {
+                if (num2 == 0.0) {
+                    Toast.makeText(baseContext, "0으로 나누면 안됩니다.", Toast.LENGTH_LONG).show()
+                    return@OnClickListener
+                }
+                result = num1 / num2
+            }
+            R.id.btnMod -> result = num1 % num2
         }
-        btnMultiply.setOnClickListener {
-            var num1 = edit1.text.toString().toInt()
-            var num2 = edit2.text.toString().toInt()
-            var result = num1 * num2
-            textResult.text = "계산 결과: " + result
-            false
-        }
-        btnDivide.setOnClickListener {
-            var num1 = edit1.text.toString().toInt()
-            var num2 = edit2.text.toString().toInt()
-            var result = num1 / num2
-            textResult.text = "계산 결과: " + result
-            false
-        }
+
+        textResult.text = "계산 결과: " + result
     }
 }
